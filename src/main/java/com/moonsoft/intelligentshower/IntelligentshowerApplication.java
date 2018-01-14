039f1220d993eaccf41945188e95fc7fce79689c
wang <excellentwanglei@hotmail.com>
2018-01-14 18:54:25 +0800

    spirng-boot-shower projcet init

---
+++ src/main/java/com/moonsoft/intelligentshower/IntelligentshowerApplication.java
@@ -0,0 +1,12 @@
+package com.moonsoft.intelligentshower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentshowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentshowerApplication.class, args);
+	}
+}
